045442c8cf6d5678bbcbdb5acb36d18e8a1cbc95
babyshout <lsms0523@gmail.com>
2024-06-20 00:51:06 +0900

    NotePadListMain.jsx -> mock data 제거

--- src/components/notepad/main/NotepadListMain.jsx
+++ src/components/notepad/main/NotepadListMain.jsx
@@ -184,7 +184,7 @@ export default function NotepadListMain({loginSessionInfo = null}) {
                         </Grid>
                     ))}
 
-
+{/*
                     <Grid item xs={12} md={4} lg={3}>
                         <Paper
                             sx={{
@@ -211,6 +211,7 @@ export default function NotepadListMain({loginSessionInfo = null}) {
                             <Notepad/>
                         </Paper>
                     </Grid>
+*/}
 
                 </Grid>
 
